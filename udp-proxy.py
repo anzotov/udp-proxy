31a8a5a0523ce1a9830af1555a6e50e59dfcfb4f
Andrey Zotov <Andrey.N.Zotov@yandex.ru>
2024-04-13 13:28:12 +0500

    Graceful stop

--- udp-proxy.py
+++ udp-proxy.py
@@ -14,42 +14,43 @@ def main():
 
     client_socket_lock = Lock()
     wsl_socket_lock = Lock()
+    run_lock = Lock()
+    run = True
 
     def client_handler():
         while True:
             data = b""
             addr = "", 0
-            client_socket_lock.acquire()
-            try:
-                data, addr = client_socket.recvfrom(4096)
-            except:
-                sleep(0.1)
-            finally:
-                client_socket_lock.release()
-            if data and addr[0] == client_addr[0]:
-                wsl_socket_lock.acquire()
+            with client_socket_lock:
                 try:
+                    data, addr = client_socket.recvfrom(4096)
+                except:
+                    sleep(0.1)
+            if data and addr[0] == client_addr[0]:
+                with wsl_socket_lock:
                     wsl_socket.sendto(data, linux_wsl_addr)
-                finally:
-                    wsl_socket_lock.release()
+            with run_lock:
+                if not run:
+                    break
 
     def wsl_handler():
         while True:
             data = b""
             addr = "", 0
-            wsl_socket_lock.acquire()
-            try:
-                data, addr = wsl_socket.recvfrom(4096)
-            except:
-                sleep(0.1)
-            finally:
-                wsl_socket_lock.release()
-            if data and addr == linux_wsl_addr:
-                client_socket_lock.acquire()
+            with wsl_socket_lock:
                 try:
+                    data, addr = wsl_socket.recvfrom(4096)
+                except:
+                    sleep(0.1)
+            if data and addr == linux_wsl_addr:
+                with client_socket_lock:
                     client_socket.sendto(data, client_addr)
-                finally:
-                    client_socket_lock.release()
+            with run_lock:
+                if not run:
+                    break
+
+    client_handler_thread = Thread(target=client_handler)
+    wsl_handler_thread = Thread(target=wsl_handler)
 
     try:
         client_socket.setblocking(False)
@@ -57,14 +58,15 @@ def main():
         client_socket.bind(win_addr)
         wsl_socket.bind(win_wsl_addr)
 
-        client_handler_thread = Thread(target=client_handler)
-        wsl_handler_thread = Thread(target=wsl_handler)
         client_handler_thread.start()
         wsl_handler_thread.start()
 
         input("Press ENTER key to stop...")
 
     finally:
+        run = False
+        client_handler_thread.join()
+        wsl_handler_thread.join()
         client_socket.close()
         wsl_socket.close()
 
